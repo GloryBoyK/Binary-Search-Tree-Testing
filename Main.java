0bd36c9b32b6241892df6834fe2d3d8935b8ae66
Kanishk <86213869+Kanishk-Pandey@users.noreply.github.com>
2022-11-07 11:04:09 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,17 @@
 class Main {
-  public static void main(String[] args) {
-    BST bst1=new BST();
-    bst1.insert(1);
-    bst1.insert(2);
-    bst1.insert(0);
-    bst1.insert(5);
-    System.out.println(bst1.inOrder());
-  }
+    public static void main(String[] args) {
+        BST bst1=new BST();
+        bst1.insert(7);
+        bst1.insert(10);
+        bst1.insert(8);
+        bst1.insert(5);
+        bst1.insert(6);
+        bst1.insert(2);
+        bst1.insert(12);
+        System.out.println(bst1.postOrder());
+        System.out.println(bst1.size());
+        bst1.clear();
+        System.out.println(bst1.size());
+        System.out.println(bst1.postOrder());
+    }
 }
